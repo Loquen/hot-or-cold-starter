6b3478ae8d9e3b34544a0787ed72a7a68373a3ed
Loquen Jones <lrjones416@gmail.com>
2014-07-29 12:17:04 -0700

    Finished first edtion

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 
 $(document).ready(function(){
-	
+	var guessCount = 0;
+	var secretNumber;
+	var guess;
+
+	//Start a new game once the document has loaded
+	newGame();
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -11,6 +17,70 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+  	$(".new").click(function(){
+  		newGame();
+  	});
+
+  	$("#guessButton").click(function(e){
+  		addGuess(e);
+  		feedback();
+  	});
+
+  	//Gives feedback about the user's guess in relation to the Secret Number
+  	function feedback() {
+  		if(guess == secretNumber){
+  			$("#feedback").text("You are correct!");
+  		} else if(Math.abs(guess - secretNumber) < 50) {
+  				$("#feedback").text("Cool!");
+  				if(Math.abs(guess - secretNumber) < 25) {
+  					$("#feedback").text("Warm!");
+  					if(Math.abs(guess - secretNumber) < 10) {
+  						$("#feedback").text("Hot!");
+  					}
+  				}
+  		} else {
+  			$("#feedback").text("Cold!");
+  		}
+  	}
+
+  	//Adds guess's to guess list and increments count
+  	function addGuess(e) {
+   		guess = $("#userGuess").val();
+  		var clear = $("#userGuess").val("");
+  		guessCount++;
+
+  		if(guess > 0 && guess <= 100) {
+  			$("#guessList").append("<li>" + guess + "</li>");
+  			clear;
+  			$("#count").text(guessCount);
+  		}
+  		
+  		e.preventDefault();
+  	}
+
+  	//Function called on click of New Game button
+  	function newGame() {
+  		//Get new secret number
+  		secretNumber = setRandomNumber();
+  		
+  		//Set guess count to 0
+  		guessCount = 0;
+
+  		//Remove all previous Guess's
+  		$("#guessList").children().hide();
+
+  		//Remove feedback
+  		$("#feedback").text("Make your Guess!");
+
+  		//Set count back to 0
+  		$("#count").text("0");
+
+  	}
+
+  	function setRandomNumber() {
+  		return(Math.floor((Math.random() * 100) + 1));
+  	}
 
 });
 
